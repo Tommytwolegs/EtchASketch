521c3b5127d1ff18362c9452866cf9f91c350d64
Tommytwolegs <tommydpowers@gmail.com>
2020-06-15 15:35:03 +0700

    adding functions to calculate row and column values. Also changed arbitrary values for a 4x4 grid to variables to be manipulated later.

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 const container = document.querySelector('#container');
 container.cssText = 'grid | inline-grid; grid-template-columns: 1fr 1fr 1fr 1fr; grid-template-rows: 1fr 1fr 1fr 1fr';
-
-for(i = 1; i <= 16; i++){
+let containerHeight = 4;
+let containerWidth = 4;
+let containerSize = containerHeight * containerWidth;
+for(i = 1; i <= containerSize; i++){
 
     const box = document.createElement('div');
     box.classList.add('box');
+    box.setAttribute('row-value', rowNumber(i));
+    box.setAttribute('column-value', columnNumber(i));
+    
     box.textContent='fish';
     box.style.cssText = 'border-width: 2px; border-style: solid;';
     box.style.cssText += 'border-color: black;';
@@ -12,3 +17,20 @@ for(i = 1; i <= 16; i++){
     container.appendChild(box);
 
 };
+
+function columnNumber(divNumber){
+    let columnNumber = containerWidth;
+    if (divNumber % containerWidth != 0){
+        columnNumber = divNumber % containerWidth;
+    }
+    return columnNumber;
+}
+
+function rowNumber(divNumber){
+    let rowNumber = 1;
+    let count = 1;
+
+    rowNumber = divNumber % 4;
+    return rowNumber;
+
+}
